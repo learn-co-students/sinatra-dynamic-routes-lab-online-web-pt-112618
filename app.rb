71ce679805e7c1b4d3a94a66a2df76ec81a1d61c
Chris Philbin <chris@chrisphilbin.net>
2019-03-12 22:45:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,9 +13,33 @@ class App < Sinatra::Base
   end
   
   get '/say/:number/:phrase' do
-    params[:number].times do
-      "#{params[:phrase]}"
+    final_string = ""
+    (params[:number].to_i).times do
+      final_string += "#{params[:phrase]}\n"
     end
+    final_string
   end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if operation == "add"
+      answer = number1 + number2
+    elsif operation == "subtract"
+      answer = number1 - number2
+    elsif operation == "multiply"
+      answer = number1 * number2
+    elsif operation == "divide"
+      answer = number1 / number2
+    else
+      answer = "Unable to perform this operation"
+    end
+    answer.to_s
+end
 
 end
