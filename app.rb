d33f9f83f957ec6c4fadc7d9e1a12bb3915e789f
norawolf <noraevogt@gmail.com>
2019-03-13 21:59:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -8,8 +8,7 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    @num = params[:number].to_i
-    "#{(@num * @num).to_s}"
+    (params[:number].to_i ** 2).to_s
   end
 
   get '/say/:number/:phrase' do
